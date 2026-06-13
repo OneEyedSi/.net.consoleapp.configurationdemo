f96337a174be7d67068118f9b1ff20a51b792522
Simon Elms <simon.elms@datacom.com>
2026-06-14 04:27:33 +1200

    REFACTOR: Move options files to their own folder

--- ConsoleAppWithConfiguration/Worker.cs
+++ ConsoleAppWithConfiguration/Worker.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using ConsoleAppWithConfiguration.ConfigurationOptions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
